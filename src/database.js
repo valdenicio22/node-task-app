0446bf0d891eb1f1f2d7e0686b0814ac9e80f765
valdenicio22 <valdenicio22@gmail.com>
2023-10-06 18:22:03 +0200

    feat: add func to handle search with filter and create new task

--- src/database.js
+++ src/database.js
@@ -1,22 +1,46 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
 
 export class Database {
   #database = {}
+
+  constructor(){
+    fs.readFile(databasePath, 'utf8')
+    .then((data) => {
+      this.#database = JSON.parse(data)
+    }).catch(() => {
+      this.#persist()
+    })
+  }
+
+  #persist(){
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
 
   verifyIfTableExists(table){
     if(Array.isArray(this.#database[table])) return true
     return false
   }
 
-  select(table){
-    const data = this.#database[table] ?? []
+  select(table, search){
+    let data = this.#database[table] ?? []
+    if(search){
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
     return data
   }
   insert(table, data){
-    if(this.verifyIfTableExists(table)){
+    if(this.verifyIfTableExists(table)){  
       this.#database[table].push(data)
     }else{
       this.#database[table] = [data]
     }
+    this.#persist()
     return data
   }
   delete(table, id){
@@ -25,16 +49,18 @@ export class Database {
 
       if(itemIndex > -1){
         this.#database[table].splice(itemIndex, 1)
+        this.#persist()
       }else{
-        return fasle
+        return false
       }
     }else return false
   }
-  update(table, id){
+  update(table, id, data){
     if(this.verifyIfTableExists(table)){
       const itemIndex = this.#database[table].findIndex(item => item.id === id)
       if(itemIndex > -1){
-        this.#database[table].splice(itemIndex, 1)
+        this.#database[table][itemIndex] = {id, ...data}
+        this.#persist()
       }else return false
     }else return false
   }
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,46 @@
-import { Database } from "./database";
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
-      const users = database.select('users')
-      return res.end(JSON.stringify(users))
+      const {search} = req.query
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search
+      } : null)
+      return res.end(JSON.stringify(tasks))
     }
-  }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const {title, description} = req.body
+
+      if (!title || !description) {
+        return response.writeHead(422).end(JSON.stringify({
+          message: "Title and description are required."
+        }));
+      }
+
+      const newTask = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+      
+      database.insert('tasks', newTask)
+      return res.writeHead(201).end()
+    }
+  },
+  
 ]
